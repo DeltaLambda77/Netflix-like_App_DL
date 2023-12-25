25fc3f99fdc97eaecb84fad427369c4b419d01fa
DeltaLambda77 <danlozano77@gmail.com>
2023-12-24 20:33:31 -0500

    Fixed import useState bug

--- P1/starter-code/Netflix_Clone/src/components/HoverableImage.jsx
+++ P1/starter-code/Netflix_Clone/src/components/HoverableImage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const HoverableImage = ({ initialIcon, hoveredIcon }) => {
     const [imageSrc, setImageSrc] = useState(initialIcon);

463724c5f5ccefeee0727b49ac2f94890f1b9f73
DeltaLambda77 <danlozano77@gmail.com>
2024-01-05 17:23:23 -0500

    Added Watchlist and ViewingHistory models

--- server/models.py
+++ server/models.py
@@ -11,15 +11,15 @@ class User(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
-    passwordHash = db.Column(db.String, nullable=False)
-    firstName = db.Column(db.String)
-    lastName = db.Column(db.String)
-    dateOfBirth = db.Column(db.datetime)
-    profilePicture = db.Column(db.String)
+    password_hash = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    date_of_birth = db.Column(db.datetime)
+    profile_picture = db.Column(db.String)
     email = db.Column(db.String)
-    preferredGenre = db.Column(db.String)
+    preferred_genre = db.Column(db.String)
     
-    viewingHistory = db.Column("ViewingHistory", back_populates="user") 
+    viewing_history = db.Column("ViewingHistory", back_populates="user") 
     watchlist = db.Column("Watchlist", back_populates="user")
 
 
@@ -33,8 +33,34 @@ class Movie(db.Model, SerializerMixin):
     rating = db.Column(db.String)
     isBookmarked = db.Column(db.Boolean, default=False)
     isTrending = db.Column(db.Boolean)
-    trendingImageSmall = db.Column(db.String)
-    trendingImageLarge = db.Column(db.String)
-    regularImageSmall = db.Column(db.String)
-    regularImageMedium = db.Column(db.String)
-    regularImageLarge = db.Column(db.String)
+    trending_image_small = db.Column(db.String)
+    trending_image_large = db.Column(db.String)
+    regular_image_small = db.Column(db.String)
+    regular_image_medium = db.Column(db.String)
+    regular_image_large = db.Column(db.String)
+    clicks = db.Column(db.Integer)
+
+class Watchlist(db.Model, SerializerMixin):
+    __tablename__ = "watchlists"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'))
+    added_at = db.Column(db.datetime, default=datetime.utcnow)
+
+    user = db.Column("User", back_populates="watchlist")
+    movie = db.Column("Movie", back_populates="watchlist")
+    
+class ViewingHistory(db.Model, SerializerMixin):
+    __tablename__ = "viewing_history"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
+    previously_watched = db.Column(db.String)
+    genre1_counter = db.Column(db.Integer)
+    genre2_counter = db.Column(db.Integer)
+    genre3_counter = db.Column(db.Integer)
+
+    user = db.Column("User", back_populates="viewing_history")
+    movie = db.Column("Movie", back_populates="viewing_history")
